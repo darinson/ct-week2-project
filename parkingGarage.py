4c8fee0409571617614833c88449c345a4c92ec8
Darin Son <dson64@outlook.com>
2021-03-28 10:46:01 -0700

    Format print statements(text & list/dict keys) -DS

--- parkingGarage.py
+++ parkingGarage.py
@@ -5,66 +5,70 @@ class parkingGarage():
         self.currentTickets = {}
 
     def takeTicket(self):
-        
+
         if self.tickets == []:
-            print("Garage is full")
+            print("Garage is full.")
             return parkingGarage
 
-        ticketnum = int(input('choose a ticket number: '))
+        print("Here the available tickets: ", self.tickets)
+        ticketnum = int(input('Choose a ticket number: '))
         if ticketnum not in self.tickets:
-            print("Ticket not available")
+            print("Ticket not available.")
             print("Here the available tickets: ", self.tickets)
             ticketnum = int(input('Please enter valid ticket number: '))
-            
-
 
         self.tickets.remove(ticketnum)
         self.parkingSpaces.remove(ticketnum)
         self.currentTickets[ticketnum] = ""
-        print("current tickets pulled: ", self.currentTickets)
-            
-    def payForParking(self):
-        ticketnum = int(input('please choose your ticket: '))
-        if ticketnum not in self.currentTickets.keys():
-            print("Ticket not found, here are the available tickets: ", ','.join([str(key) for key in self.currentTickets.keys()]))
-            ticketnum = int(input('please enter valid ticket: '))
+        print("Current tickets pulled: [{}]".format(
+            ','.join([str(key) for key in self.currentTickets.keys()])))
 
-        amount = (input('please enter payment amount of $20: '))
-        #if isinstance(self.amount, str):
-            #amount = input('Please enter a number: ')
+    def payForParking(self):
+        ticketnum = int(input('Please choose your ticket: '))
+        if ticketnum not in self.currentTickets.keys():
+            print("Ticket not found, here are the available tickets: [{}]".format(
+                  ','.join([str(key) for key in self.currentTickets.keys()])))
+            ticketnum = int(input('Please enter valid ticket: '))
+
+        amount = (input('Please enter payment amount of $20: '))
+        # if isinstance(self.amount, str):
+        # amount = input('Please enter a number: ')
         if float(amount) == 0 or float(amount) < 20:
-            amount = float(input('please enter a valid amount: '))
+            amount = float(input('Please enter a valid amount: '))
 
         if ticketnum in self.currentTickets.keys():
             self.currentTickets[ticketnum] = "Paid"
             print("Ticket has been paid. You now have 15 minutes to leave.")
-    
+
     def leaveGarage(self):
-        ticketnum = int(input('please enter your ticket number:'))
+        ticketnum = int(input('Please enter your ticket number: '))
         if ticketnum not in self.currentTickets.keys():
-            print(self.currentTickets.keys())
-            ticketnum = int(input('please enter valid ticket'))
+            print("Ticket not found, here are the available tickets: [{}]".format(
+                  ','.join([str(key) for key in self.currentTickets.keys()])))
+            ticketnum = int(input('Please enter a valid ticket: '))
         if self.currentTickets[ticketnum] != "Paid":
-            amount = float(input('Ticket has not been paid, please enter a payment of $20:'))
+            amount = float(
+                input('Ticket has not been paid, please enter a payment of $20:'))
            # if isinstance(self.amount, str):
             #    amount = input('Please enter a number: ')
-            if float(amount) == 0 or float(amount) < 20 :
-                amount = float(input('please enter the valid amount: '))
+            if float(amount) == 0 or float(amount) < 20:
+                amount = float(input('Please enter the valid amount: '))
         del self.currentTickets[ticketnum]
 
-        print("Thank you, have a nice day")
+        print("Your ticket has been paid.\nThank you, have a nice day")
         self.tickets.append(ticketnum)
         self.tickets = sorted(self.tickets)
         self.parkingSpaces.append(ticketnum)
         self.parkingSpaces = sorted(self.parkingSpaces)
-         
+
+
 myGarage = parkingGarage()
 
 
 def runGarage():
     while True:
         response = input(
-            'what would you like to do? take ticket, pay or leave? ')
+            'What would you like to do? Take ticket, pay or leave? ')
         if response.lower() == 'take ticket':
             myGarage.takeTicket()
         elif response.lower() == 'pay':
@@ -72,8 +76,8 @@ def runGarage():
         elif response.lower() == 'leave':
             myGarage.leaveGarage()
            # if myGarage.currentTickets == {}:
-               # print("All tickets paid, good bye!")
-               # break
-        
-            
+            # print("All tickets paid, good bye!")
+            # break
+
+
 runGarage()
